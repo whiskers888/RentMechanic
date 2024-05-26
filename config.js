54ebf5a32a18539186cc35ed3ed9a10228d24204
a-zelenkov <zelenkov_a_m_01@mail.ru>
2024-05-26 21:25:41 +0300

    fix

--- config.js
+++
@@ -1,6 +0,0 @@
-// module.exports = {
-// 	config: {
-// 		tailwindjs: "./tailwind.config.js",
-// 		port: 3000
-// 	}
-// }
--- src/js/libs/burger.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.burger').addEventListener('click', function(){
-    document.querySelector('.burger span').classList.toggle('active');
-    document.querySelector('.menu_mob').classList.toggle("animate");
-
-})
-
